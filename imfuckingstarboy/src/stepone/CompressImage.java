2ad7e0530d0f7a7d11aa1ac5f4b5d7fd434448ad
libo <libo@genius-PC>
2018-04-16 17:08:31 +0800

    asd

--- imfuckingstarboy/src/stepone/CompressImage.java
+++ imfuckingstarboy/src/stepone/CompressImage.java
@@ -16,6 +16,7 @@ public class CompressImage {
 
 	public static void qqweqwe() {
 		System.out.println();
+		System.out.println(1);
 	}
 
 	public static void qqq(final String filePath) throws IOException {
